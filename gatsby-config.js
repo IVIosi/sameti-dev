84dd29223dedec6ab66e43d66ca8f501377b57fd
Mostafa <mos.sameti@gmail.com>
2020-03-20 23:32:39 +0330

    initial commit and add project configuration

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    title: `Mostafa Sameti Website`,
+    description: `Mostafa Sameti Personal Website`,
+    author: `mostafa-sameti`,
+  },
+  plugins: [`gatsby-plugin-sass`],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/main.scss";
+
+const IndexPage = () => (
+  <p>
+      index
+  </p>
+)
+
+export default IndexPage
