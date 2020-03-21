1e70c614570606458f8efa542a53e542dabedb5e
Mostafa <mos.sameti@gmail.com>
2020-03-22 02:44:58 +0430

    add reusable page layout component

---
+++ src/components/layout.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from "react"
+
+import "../styles/main.scss"
+import InfoCard from "./info-card"
+import Copyright from "./copyright"
+import Navigation from "./navigation"
+
+export default function Layout({ children }) {
+  return (
+    <Fragment>
+      <div className="polygon polygon-left" />
+      <div className="polygon polygon-right" />
+      <main className="main">
+        <InfoCard gravatar="b3809a056ac56a369c1d823bbf93cddc" />
+        {children}
+        <Navigation />
+      </main>
+      <footer className="footer">
+        <Copyright />
+      </footer>
+    </Fragment>
+  )
+}
