07c954988be951a86de3e4a3e43386e412aa2ee1
Mostafa <mos.sameti@gmail.com>
2020-03-21 23:44:26 +0430

    add index page structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,23 @@
-import React from "react";
-import "../styles/main.scss";
+import React, { Fragment } from "react"
 
-const IndexPage = () => (
-  <p>
-      index
-  </p>
-)
+import "../styles/main.scss"
+import InfoCard from "../components/info-card"
+import Content from "../components/content"
+import Copyright from "../components/copyright"
 
-export default IndexPage
+export default function IndexPage() {
+  return (
+    <Fragment>
+      <div className="polygon polygon-left" />
+      <div className="polygon polygon-right" />
+      <main className="main">
+        <InfoCard gravatar="b3809a056ac56a369c1d823bbf93cddc" />
+        <Content />
+        <nav className="navbar">icons</nav>
+      </main>
+      <footer className="footer">
+        <Copyright />
+      </footer>
+    </Fragment>
+  )
+}
