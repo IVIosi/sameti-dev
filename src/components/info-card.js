04e584ec501df09e7081c0874c1d1c30fb3f7b8f
Mostafa <mos.sameti@gmail.com>
2020-03-21 23:46:23 +0430

    add info card component

---
+++ src/components/info-card.js
@@ -0,0 +1,61 @@
+import React from "react"
+import MediumIcon from "../static/medium.svg"
+import GithubIcon from "../static/github.svg"
+import TwitterIcon from "../static/twitter.svg"
+import MailIcon from "../static/mail.svg"
+import LinkedinIcon from "../static/linkedin.svg"
+
+export default function InfoCard({ gravatar }) {
+  const socials = [
+    {
+      name: "Medium",
+      logo: MediumIcon,
+      link: "",
+    },
+    {
+      name: "LinkedIn",
+      logo: LinkedinIcon,
+      link: "https://www.linkedin.com/in/mostafasameti",
+    },
+    {
+      name: "Email",
+      logo: MailIcon,
+      link: "mailto:mos.sameti@Gmail.com",
+    },
+    {
+      name: "Twitter",
+      logo: TwitterIcon,
+      link: "https://twitter.com/mostafa_sameti",
+    },
+    {
+      name: "GitHub",
+      logo: GithubIcon,
+      link: "https://github.com/IVIosi",
+    },
+  ]
+
+  return (
+    <aside className="info-card">
+        <div className="info-card__avatar">
+          <img
+            src={`https://www.gravatar.com/avatar/${gravatar}?s=100`}
+            alt="Mostafa"
+          />
+        </div>
+        <p className="info-card__name bold">Mostafa Sameti</p>
+        <p className="info-card__job">Front-end Developer</p>
+        <ul className="info-card__social">
+          {socials.map(item => {
+            const IconName = item.logo
+            return (
+              <li key={item.name} title={item.name}>
+                <a href={item.link} target="_blank" rel="noopener noreferrer">
+                  <IconName className="info-card__social-icon" />
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+    </aside>
+  )
+}
