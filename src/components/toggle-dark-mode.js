fb9e5bab9e40371c268663d2e687f52bb7ea7eeb
Mostafa <mos.sameti@gmail.com>
2020-03-29 20:39:59 +0430

    refactor toggle dark mode

--- src/components/toggle-dark-mode.js
+++ src/components/toggle-dark-mode.js
@@ -1,36 +1,54 @@
-import React, { useEffect } from "react"
+import React from "react"
 
 import MoonIcon from "../static/moon.svg"
 import SunIcon from "../static/sun.svg"
 
-export default function ToggleDarkMode() {
-  let initialTheme = "light"
-  let setTheme = {}
+export default  class ToggleDarkMode extends React.Component {
+  constructor(props) {
+    super(props)
 
-  useEffect(() => {
-    initialTheme = localStorage.getItem("theme")
-    setTheme = theme => {
-      localStorage.setItem("theme", theme)
-      document.querySelector("body").setAttribute("class", theme)
+    this.state = {
+      theme: "light",
     }
-    setTheme(initialTheme)
-  });
 
-  const handleToggle = e => {
-    setTheme(!e.target.checked ? "light" : "dark")
+    this.handleToggle = this.handleToggle.bind(this)
   }
-  return (
-    <label className="switch">
-      <div className="switch__icon">
-        <MoonIcon />
-        <SunIcon />
-      </div>
-      <input
-        defaultChecked={initialTheme === "dark"}
-        type="checkbox"
-        onChange={handleToggle}
-      />
-      <span className="switch__slider" />
-    </label>
-  )
+
+  componentDidMount() {
+    const localTheme = localStorage.getItem("theme")
+    if (localTheme) {
+      this.setState({
+        theme: localTheme
+      })
+      document.querySelector("body").setAttribute("class", localTheme)
+    } else {
+      localStorage.setItem("theme", "light")
+    }
+  }
+
+  handleToggle(e) {
+    const newTheme = e.target.checked ? "dark" : "light"
+    this.setState({
+      theme: newTheme
+    })
+    localStorage.setItem("theme", newTheme)
+    document.querySelector("body").setAttribute("class", newTheme)
+  }
+
+  render() {
+    return (
+      <label className="switch">
+        <div className="switch__icon">
+          <MoonIcon />
+          <SunIcon />
+        </div>
+        <input
+          checked={this.state.theme === "dark"}
+          type="checkbox"
+          onChange={(e) => this.handleToggle(e)}
+        />
+        <span className="switch__slider" />
+      </label>
+    )
+  } 
 }
